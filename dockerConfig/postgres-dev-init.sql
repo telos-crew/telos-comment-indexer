0876e60b38f923875df83139969613e446eec08b
Kylan Hurt <kylan.hurt@gmail.com>
2023-04-13 17:46:37 -0300

    Rename adonis_app to comments in dockerConfig

--- dockerConfig/postgres-dev-init.sql
+++ dockerConfig/postgres-dev-init.sql
@@ -1,3 +1,3 @@
 CREATE USER comment with encrypted password 'S4pient!postgres';
-CREATE DATABASE adonis_app;
-GRANT ALL PRIVILEGES ON DATABASE adonis_app TO adonis;
+CREATE DATABASE comment;
+GRANT ALL PRIVILEGES ON DATABASE comment TO adonis;
