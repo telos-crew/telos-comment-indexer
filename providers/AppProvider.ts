7a2498e44e7eaaa93b7f7f493876aae0a0aa420b
Kylan Hurt <kylan.hurt@gmail.com>
2023-09-21 16:01:48 -0300

    Remove need to query blockchain

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -1,6 +1,5 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
 import { replenishDstorAccessToken } from '../util'
-import { query } from '../util/hyperion'
 
 export default class AppProvider {
   constructor(protected app: ApplicationContract) {}
@@ -11,8 +10,6 @@ export default class AppProvider {
 
   public async boot() {
     // IoC container is ready
-    const { default: Database } = await import('@ioc:Adonis/Lucid/Database')
-    setInterval(() => query('testcomments', Database), 1000)
     replenishDstorAccessToken()
   }
 
