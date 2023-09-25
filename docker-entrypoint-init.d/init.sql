608f1479e7321f111de885d3e73fc6c7c1966e8b
Kylan Hurt <kylan.hurt@gmail.com>
2023-09-25 15:07:22 -0300

    Add some docker file stuff

---
+++ docker-entrypoint-init.d/init.sql
@@ -0,0 +1,12 @@
+services:
+  postgres:
+    container_name: postgres
+    image: postgres:13
+    volumes:
+      - postgres_volume:/var/lib/postgresql/data
+      - ./dockerConfig/postgres-dev-init.sql:/docker-entrypoint-initdb.d/init.sql # will setup dev database adonis_app for us
+    environment:
+      POSTGRES_USER: comment
+      POSTGRES_PASSWORD: 'somepassword'
+    ports:
+      - 5432:5432
